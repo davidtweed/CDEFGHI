cc66af32abca5186c092495f42e40eec72a065d8
David Tweed <david.tweed@gmail.com>
2012-10-21 14:12:40 +0100

    More minor parser work

--- parser.hs
+++ parser.hs
@@ -15,13 +15,15 @@ type Type=String
 data ArrExp=Arr String [String] Bool deriving (Eq,Show)
 --type Asgn=(Asgn',OptTy) -- assignment with optional LHS type
 data Op=Op String OptTy deriving (Eq,Show) -- operator with type it occurs in
+type RHSArrExp=Either ArrExp String -- Right number
 type OptTy=Maybe Type -- optional type annoation
-data Stmt=Inline [ArrExp] [ArrExp]
+data Stmt={-Inline [ArrExp] [ArrExp]
    | Sgl ArrExp
-   | Where ArrExp String
+   |-}
+     Where ArrExp String
    | EBlk
-   | Cpd ArrExp OptTy (Maybe Char) [ArrExp] [Op]
-   | MacroProto [String] String [String]
+   | Cpd ArrExp OptTy (Maybe Char) [(Op,RHSArrExp)]
+   | MacroProto [String] {-String [String]-}
    | MacroUse [(String,Bool)] String [String] -- name out parameters name inparameters
    deriving (Eq,Show)
 
@@ -39,24 +41,39 @@ fl p1 p2 cs=case p1 cs of
        Nothing->Nothing
        Just(v,cs'')->Just(f v,cs'')
 
-flB::Parser (Bool->b)->Parser a->Parser b
-flB p1 p2 cs=case p1 cs of
+fSyntax::Parser a->Parser b->Parser a
+fSyntax a b c=case a c of
   Nothing->Nothing
-  Just(f,cs')->case p2 cs' of
+  r@(Just(v,c'))->case b c' of
+    Nothing->r
+    Just(_,c'')->Just(v,c'')
+
+flB::Parser (Bool->b)->Parser a->Parser b
+flB p1 p2 cs=case p1 (dropWS' cs) of
+  Nothing->Nothing
+  Just(f,cs')->case p2 (dropWS' cs') of
        Nothing->Just(f False,cs')
        Just(v,cs'')->Just(f True,cs'')
 
 flM::Parser (Maybe a->b)->Parser a->Parser b
-flM p1 p2 cs=case p1 cs of
+flM p1 p2 cs=case p1 (dropWS' cs) of
   Nothing->Nothing
-  Just(f,cs')->case p2 cs' of
+  Just(f,cs')->case p2 (dropWS' cs') of
        Nothing->Just(f Nothing,cs')
        Just(v,cs'')->Just(f (Just v),cs'')
 
 flL::Parser ([a]->b)->Parser a->Parser b
-flL p1 p2 cs=case p1 cs of
+flL p1 p2 cs=case p1 (dropWS' cs) of
   Nothing->Nothing
-  Just(f,cs')->let (Just(v,cs''))=pPEList p2 cs' in Just(f v,cs'')
+  Just(f,cs')->let (Just(v,cs''))=pPEList pNull p2 cs' in Just(f v,cs'')
+
+flSL::Parser c->Parser ([a]->b)->Parser a->Parser b
+flSL pSep p1 p2 cs=case p1 (dropWS' cs) of
+  Nothing->Nothing
+  Just(f,cs')->let (Just(v,cs''))=pPEList pSep p2 cs' in Just(f v,cs'')
+
+pSpc=pNull
+pComma=pChar ','
 
 (@>):: Parser a->Parser b->Parser b
 p @> q = (p `pSeq` q) `raise` snd
@@ -64,28 +81,30 @@ p @> q = (p `pSeq` q) `raise` snd
 p <@ q = (p `pSeq` q) `raise` fst
 
 pBListArrExps::Bool->Parser [ArrExp]
-pBListArrExps lhs=(pChar (=='[')) @> (pNEList pEntry) <@ (pChar (==']'))
+pBListArrExps lhs=(pChar '[') @> (pNEList pEntry) <@ (pChar ']')
   where pEntry=if lhs then pLHSIdxExp else pIdxExp
 
-pMacroUse=(pCons MacroUse) `flL` pLHS <@ pMatchStr "=" `fl` pString `flL` pString
-  where pLHS=(pString `raise` (,)) `flB` (pPredChar (conceptual_marker==) (\_->True))
+pMacroUse=(pCons MacroUse) `cSL` pLHS <@ pMatchStr "=" `fl` pString `cSL` pString
+  where pLHS=(pString `raise` (,)) `flB` ((pChar conceptual_marker) `raise` (\_->True))
+        cSL=flSL pComma
 
-pDefinition = (pMatchStr "define") @> (pCons MacroProto) `flL` pString <@ pMatchStr "=" `fl` pString `flL` pString <@ pMatchStr "{"
-
+pDefinition = (pMatchStr "define") @> (pCons MacroProto) `cSL` pString -- <@ pMatchStr "=" `fl` pString `flL` pString <@ pMatchStr "{"
+  where cSL=flSL pComma
 tryParserList :: [Parser a]-> Parser a
 tryParserList [] _ = Nothing
 tryParserList (p:ps) cs=case p cs of
   Nothing ->tryParserList ps cs
   r@(Just(_,_))->r
 
-pAnything :: Parser ()
-pAnything cs=Just((),[])
+pAll :: Parser ()
+pAll cs=Just((),[])
+pNull cs=Just((),cs)
 
 pLineParser::[Parser a]-> Parser a
 pLineParser ps cs=(tryParserList ps) (dropWS' cs) --((tryParserList ps) `flB` pCommentToEOL) (dropWS' cs)
 -- a matching "to end-of-line" comment parser just returns the incoming parse-value
 pCommentToEOL :: Parser ()
-pCommentToEOL = (dropWS (pMatchStr "##" <@ pAnything))
+pCommentToEOL = (dropWS (pMatchStr "##" <@ pAll))
 
 --to be safe we require everything on the line to have been understood to say that we've parsed the line
 allAccounted::Parser a->String->Maybe a
@@ -93,21 +112,21 @@ allAccounted p cs=case p cs of
   Nothing->Nothing
   Just(v,r)->if all isSpace r then Just v else Nothing
 
-pLine=pLineParser [pEBlk,pWhere,pDefinition,pMacroUse,pStatement]
+pLine=pLineParser [pEBlk,pWhere,{-pDefinition,-}pMacroUse,pStatement]
 
-pCharStr prd=(pChar prd) `raise` (\x->[x])
+pCharStr prd=(pPredChar' prd) `raise` (\x->[x])
 --pWhere::Parser Stmt
 pWhere=(pMatchStr "where") @> (pCons Where) `fl` pIdxExp <@ (pMatchStr "=>") `fl` (pCharStr isGenAlpha) <@ (pMatchStr "{")
 
-pEBlk = (pCons EBlk) <@ (pChar (=='}'))
+pEBlk = (pCons EBlk) <@ (pChar '}')
 
 pMatchStr::String->Parser ()
 pMatchStr xs cs={-dropWS-} (if xs `isPrefixOf` cs then Just ((),drop (length xs) cs) else Nothing)
 isPrefixOf xs ys= xs==(take (length xs) ys)
 
 fmt::Stmt->String
-fmt (Sgl a)="Single "++show a++";"
-fmt (Cpd l a t as os)="Cmpd"++show l++" "++show a++" "++show t++" "++show as++show os++";"
+--fmt (Sgl a)="Single "++show a++";"
+fmt (Cpd l a t as)="Cmpd"++show l++" "++show a++" "++show t++" "++show as++";"
 
 type Parser a=String->Maybe(a,String)
 
@@ -117,21 +136,6 @@ numberLines s=zip [1..] (lines s)
 dropWS::Parser a->Parser a
 dropWS p cs=p (dropWhile isSpace cs)
 dropWS' cs=dropWhile isSpace cs
-
-pOptPostMod::Parser a->Parser (a->a)->Parser a
-pOptPostMod p1 p2 s=case p1 s of
- Nothing->Nothing
- r@(Just (v,s'))->case p2 s' of
-              Nothing->r
-              Just (f,s'')->Just(f v,s'')
-
-pZOrMoreMod::Parser a->Parser (a->a)->Parser a
-pZOrMoreMod p q cs=case p cs of
- Nothing->Nothing
- Just (v,cs')->f v cs'
-    where f v s=case q s of
-                Nothing   ->Just (v,s)
-                Just (g,t)->f (g v) t
 
 pPredChar::(Char->Bool)->(Char->a)->Parser a
 pPredChar pred upd []=Nothing
@@ -148,17 +152,19 @@ pPredChar' pred cs@(c:cs')
 
 --parse a string into an ArrExp identifier (everything starts off virtual)
 pIdent::Parser ArrExp
-pIdent=raise pString (\cs->Arr cs [] False)
+pIdent=raise pNString (\cs->Arr cs [] False)
+
+--parse an number or identifier like string required to start with alphabetical character
+pNString::Parser String
+pNString=pNEList (pPredChar' isGenAlphaNum)
 
 --parse an "identifier-like" string required to start with alphabetical character
-pNString::Parser String
-pNString=pNEList (pChar isGenAlphaNum)
+pString,pGenNumber::Parser String
+pString=(pPredChar isGenAlpha (\x y->(x:y))) `flL` (pPredChar' isGenAlphaNum)
+pGenNumber=(pPredChar (\c->c>='0' && c<='9') (\x y->(x:y))) `flL` (pPredChar' isGenAlphaNum)
 
-pString::Parser String
-pString=(pPredChar isGenAlpha (\x y->(x:y))) `flL` (pChar isGenAlphaNum)
-
-pChar::(Char->Bool)->Parser Char
-pChar f (c:cs) | f c=Just(c,cs)
+pChar::Char->Parser Char
+pChar f (c:cs) | f==c=Just(c,cs)
 pChar _ _ = Nothing
 
 pPredCharUpd::(Char->Bool)->(Char->a->a)->Parser (a->a)
@@ -166,23 +172,30 @@ pPredCharUpd pred upd []=Nothing
 pPredCharUpd pred upd cs@(c:cs')
  |pred c   =Just(upd c,cs')
  |otherwise=Nothing
-
+{-
+pPEList::(Parser l,Parser s,Parser r)->Parser a->Parser [a]
+pPEList (l,s,r) p cs=l @>
+  where
+   pPEList' c0=case (pSep @> p) c0 of
+     Nothing->Just([],c0)
+     Just(v,c1)->let (Just(v',c2))=pEList' c2 in Just(v:v',c2)
+-}
 --parse possibly empty list
-pPEList::Parser a->Parser [a]
-pPEList p cs=Just(case p cs of
+pPEList::Parser b->Parser a->Parser [a]
+pPEList pSep p cs=Just(case p cs of
   Nothing->([],cs)
-  Just(v,cs')->let (Just(vs,cs''))=pPEList p cs' in (v:vs,cs'')
+  Just(v,cs')->let (Just(vs,cs''))=pPEList pSep p cs' in (v:vs,cs'')
   )
 --parse list which must have at least one entry
 pNEList::Parser a->Parser [a]
 pNEList p cs=case p cs of
   Nothing->Nothing
-  Just(v,cs')->let (Just(vs,cs''))=pPEList p cs' in Just(v:vs,cs'')
+  Just(v,cs')->let (Just(vs,cs''))=pPEList pNull p cs' in Just(v:vs,cs'')
 
 --slotIn::Parser a->(a->b->b)->Parser b
 pIdxExpBase::Parser (Bool->ArrExp)
-pIdxExpBase=(pCons Arr) `fl` pString `fl` ((pPredChar' (array_index_sep==)) @> pIdxs)
-  where pIdxs=(pPEList (pCharStr isGenAlphaNum))
+pIdxExpBase=(pCons Arr) `fl` pString `fSyntax` (pChar array_index_sep) `fl` pIdxs
+  where pIdxs=(pPEList pNull (pCharStr isGenAlphaNum))
 
 pIdxExp::Parser ArrExp
 pIdxExp=pIdxExpBase `fl` (pCons False)
@@ -190,7 +203,7 @@ pIdxExp=pIdxExpBase `fl` (pCons False)
 array_index_sep='.'
 conceptual_marker='!'
 pLHSIdxExp::Parser ArrExp
-pLHSIdxExp=pIdxExpBase `flB` (pChar (conceptual_marker==))
+pLHSIdxExp=pIdxExpBase `flB` (pChar conceptual_marker)
 
 pPostMod::Parser a->Parser (a->a)->Parser a
 pPostMod p q cs=case p cs of
@@ -200,7 +213,7 @@ pPostMod p q cs=case p cs of
                    Just(f,cs'')->Just (f v,cs'')
 
 pAsgnOp::Parser (Maybe Char)
-pAsgnOp=(pChar ('='==)) @> (pCons id) `flM` (pChar (`elem` "+*&|"))
+pAsgnOp=(pChar '=') @> (pCons id) `flM` (pPredChar' (`elem` "+*&|"))
 
 --should sequencing allow discardable whitespace between tokens
 pSeqIn::Bool->Parser a->Parser b->Parser (a,b)
@@ -219,41 +232,42 @@ raise::Parser a->(a->b)->Parser b
 raise p f cs=case p cs of
   Nothing->Nothing
   Just(v,cs')->Just(f v,cs')
-{-
+
 pOptionalBracketed::Char->Char->Parser a->Parser a
+pOptionalBracketed l r p []=Nothing
 pOptionalBracketed l r p cs@(c:cs')=let bracketed=(l==c) in
   case p (if bracketed then cs' else cs) of
-    Nothing->Nothing
-    Just(v,cs'')->if bracketed && not(r==head cs'') then
--}
+        Nothing->Nothing
+        Just(v,cs'')->if bracketed then if null cs'' || r/=head cs'' then Nothing else Just(v,tail cs'')
+                      else Just(v,cs'')
+
+pORB=pOptionalBracketed '(' ')'
+pOSB=pOptionalBracketed '[' ']'
 -- type annotation is eg, ':Type128'
 pTypeAnnote::Parser Type
 pTypeAnnote = (dropWS ((pPredChar' (==':')) @> (dropWS pString)))
 
 pTyBinOp::Parser Op
-pTyBinOp = (pCons Op) `fl` ((pChar (`elem` "+*/&|")) `raise` (\x->[x])) `flM` pTypeAnnote
+pTyBinOp = dropWS(pORB ((pCons Op) `fl` ((pPredChar' (`elem` "+-*/&|^2<>")) `raise` (\x->[x])) `flM` pTypeAnnote))
+
+pEAlt::Parser a->Parser b->Parser (Either a b)
+pEAlt a b c=case a c of
+   Nothing->case b c of
+      Nothing->Nothing
+      Just(w,c'')->Just(Right w,c'')
+   Just(v,c')->Just (Left v,c')
+
+pRHSArrExp = pEAlt pIdxExp pGenNumber
 
 --one odd syntax <-> AST mismatch: write LHS types on LHS identifier but store on the assignment operator
-
+--also need to stick a dummy operator on front of list
 pStatement::Parser Stmt
-pStatement=(pSimple `pZOrMoreMod` (dropWS pBinOp)) `raise` canonicalise
+pStatement=(pCons Cpd) `fl` pLHSIdxExp `flM` pTypeAnnote  `fl` pAsgnOp `fl` (pCombOps)
   where
-   pOptTy::Parser OptTy
-   pOptTy c=case pTypeAnnote c of
-       Nothing->Just(Nothing,c)
-       (Just(t,c'))->Just(Just t,c')
-   pSimple::Parser Stmt
-   pSimple=(pLHSIdxExp `pSeq` pOptTy `pSeq` pAsgnOp `pSeq` pIdxExp) `raise` (\ (((e1,t),a),e2)->Cpd e1 t a [e2] [])
-   pCombOp::Parser (Op,ArrExp)
-   pCombOp=pTyBinOp `pSeq` pIdxExp
-   pBinOp=pCombOp `raise` (\ (op,idExpr) (Cpd l a t es os)->Cpd l a t (idExpr:es) (op:os))
-   canonicalise x@(Sgl _)=x
-   canonicalise (Cpd l a t es os)=Cpd l a t (reverse es) (reverse os)
-{-
+   pCombOp::Parser (Op,RHSArrExp)
+   pCombOp=pTyBinOp `pSeq` pRHSArrExp
+   pCombOps=(pRHSArrExp `raise` (\x->((Op "X" Nothing,x):))) `flL` pCombOp
 
-pStatement::Parser Stmt
-pStatement=(pCons Cpd) `fl` pLHSIdxExp `flM` pTypeAnnote  `fl` pAsgnOp `flL` pIdxExp
--}
 processStrB::String->String
 processStrB=unlines.map (show.fromJust.(allAccounted pLine)).lines
 
@@ -269,4 +283,4 @@ processStrE=unlines.map (show.pLine).(filter (not.all isSpace) .lines)
 pFileE s=do j<-readFile s
             (putStr.processStrE) j
 
-pp=pFileE "TESTS/parseT1.txt"
+pp n=pFileE ("TESTS/parseT"++show n++".txt")
