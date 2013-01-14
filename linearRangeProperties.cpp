d9485ac1fda6ad6524d3e58b91fa80d353dd6837
David Tweed <david.tweed@gmail.com>
2013-01-14 10:00:44 +0000

    Some work on range property functions

--- linearRangeProperties.cpp
+++ linearRangeProperties.cpp
@@ -53,7 +53,37 @@ inline void f (int noElts,void *record,const F &fn) {
     }
 }
 
-template<typename T,int FIN_SZ>
-inline T rangeLookup(T** tbl,int start,int end) {
-    return T[start][FIN_SZ*start];
+/*
+ * assumes first row filled in
+ * TODO: deal with SIMD issues
+ */
+template<typename T,F>
+inline
+void formTable(T** elts,int len,int depth,F combiner) {
+    int delta=1;
+    int i=1;
+    do{
+        for(j=0;j<len-delta;++j){
+            elts[i][j]=combiner(elts[i-1][j],elts[i-1][j+delta]);
+        }
+        ++i;
+        delta *= 2;
+    }while(i<depth);
 }
+
+void formFwdTable(uint8_t** elts,int len,int depth) {
+    formTable(elts,len,[] (uint8_t x,uint8_t y) { return std::min(x,y);});
+}
+
+void formBwdTable(uint8_t** elts,int len,int depth) {
+    formTable(elts,len,[] (uint8_t x,uint8_t y) { return std::max(x,y);});
+}
+
+void formUnionTable(uint8_t** elts,int len,int depth) {
+    formTable(elts,len,[] (uint8_t x,uint8_t y) { return x | y;});
+}
+
+void formIntersectTable(uint8_t** elts,int len,int depth) {
+    formTable(elts,len,[] (uint8_t x,uint8_t y) { return x & y;});
+}
+
--- linearRangeProperties.h
+++ linearRangeProperties.h
@@ -3,10 +3,16 @@
 
 #include "smallBitvectors.h"
 
+void formFwdTable(uint8_t ** elts,int len,int depth);
 
+void formBwdTable(uint8_t ** elts,int len,int depth);
+
+void formUnionTable(uint8_t ** elts,int len,int depth);
+
+void formIntersectTable(uint8_t ** elts,int len,int depth);
 
 template<typename T,typename F,int FIN_SZ>
-inline T rangeLookup(T** tbl,int start,int end) {
+inline T valueOverRange(T** tbl,int start,int end) {
     int depth=log2[end-start];
     return F(T[depth][FIN_SZ*start],T[depth][FIN_SZ*(end-(1<<depth))]);
 }
